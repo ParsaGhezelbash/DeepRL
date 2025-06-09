9975509f804e6e6886b55b394f784157ad693da9
Parsa Ghezelbash <119513155+ParsaGhezelbash@users.noreply.github.com>
2025-06-10 02:40:48 +0430

    changed gym to gymmnasium in task 2

--- HW/HW 10/Task 2 Random Network Distillation/main.py
+++ HW/HW 10/Task 2 Random Network Distillation/main.py
@@ -1,7 +1,7 @@
 # main.py
 # Main training/evaluation entry point for PPO + RND agent in MiniGrid environments.
 
-import gym
+import gymnasium as gym
 import numpy as np
 import torch
 from Core.ppo_rnd_agent import Brain
